787395af99d6d34a3167791b646c840eda8a3dba
George Iniatis <jogeo98@hotmail.com>
2021-07-26 17:04:18 +0300

    Simple example

---
+++ index.py
@@ -0,0 +1,37 @@
+import tornado.web
+import tornado.ioloop
+
+
+class basicRequestHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write("Hello world!")
+
+
+class staticRequestHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render("index.html")
+
+
+class queryStringRequestHandler(tornado.web.RequestHandler):
+    def get(self):
+        number = int(self.get_argument("argument"))
+        result = "even" if number % 2 == 0 else "odd"
+        self.write(f"The number is {result}!")
+
+
+class resourceRequestHandler(tornado.web.RequestHandler):
+    def get(self,id):
+        self.write(f"You have requested the book with id: {id}")
+
+
+if __name__ == "__main__":
+    app = tornado.web.Application([
+        (r"/", basicRequestHandler),
+        (r"/welcome", staticRequestHandler),
+        (r"/isEven", queryStringRequestHandler),
+        (r"/books/([0-9]+)", resourceRequestHandler),
+    ])
+
+    app.listen(8888)
+    print("Listening")
+    tornado.ioloop.IOLoop.current().start()
